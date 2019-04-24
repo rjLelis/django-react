c5f76068bdcc6ed5629726617bffe58e89c2d2ad
rjLelis <renatojlelis@gmail.com>
2019-04-23 22:29:30 -0300

    Create get and post api method for leads

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from .models import Lead
+from rest_framework import generics
+from .serializers import LeadSerializer
 
-# Create your views here.
+class LeadListCreate(generics.ListCreateAPIView):
+    queryset = Lead.objects.all()
+    serializer_class = LeadSerializer
