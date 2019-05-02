2400e1c235a059b1417e53a54b96d8a636e1fddf
rjLelis <renatojlelis@gmail.com>
2019-05-02 13:06:16 -0300

    Table is now updating on post

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,16 +1,57 @@
 import React from 'react';
 import ReactDom from 'react-dom'
-import DataProvider from './DataProvider';
 import Table from './Table'
 import Form from './Form'
 
-const App = () => (
-    <React.Fragment>
-        <DataProvider endpoint='api/lead/'
-            render={data => <Table data={data} />} />
-        <Form endpoint="api/lead/" />
-    </React.Fragment>
-);
+class App extends React.Component {
+
+    state = {
+        data: [],
+        loaded: false,
+        placeholder: 'Loading...'
+    }
+
+    componentDidMount() {
+        fetch('/api/lead/').then(response => {
+            if(response.status !== 200) {
+                return this.setState({ placeholder: 'Something went wrong!'});
+            }
+            return response.json();
+        })
+        .then(data => this.setState({ data: data , loaded: true}));
+    }
+
+    handleSubmit = (e, lead) => {
+        e.preventDefault();
+        const conf = {
+            method: "POST",
+            body: JSON.stringify(lead),
+            headers: new Headers({ "Content-type": "application/json" })
+        };
+        fetch('api/lead/', conf).then(response => {
+            if(response.status !== 200) {
+                return this.state({ placeholder: 'Error on post' });
+            }
+            return (fetch('api/lead/').then(response => {
+                if(response.status !== 200) {
+                    return this.setState({ placeholder: 'Something went wrong!'});
+                }
+                return response.json();
+            })
+            .then(data => this.setState({ data: data, loaded: true })))
+        });
+    }
+
+    render() {
+        const {loaded, placeholder} = this.state;
+        return loaded ? (
+            <React.Fragment>
+                <Table data={this.state.data}/> 
+                <Form onSubmit={this.handleSubmit}/>
+            </React.Fragment>
+        ) : (<p>{placeholder}</p>)
+    }
+}
 
 const wrapper = document.getElementById('app');
 
