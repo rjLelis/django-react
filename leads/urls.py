efa85c860969a3a167af4c6878e4f39394691608
rjLelis <renatojlelis@gmail.com>
2019-05-02 13:00:17 -0300

    Changed the url to point to a functional api function

--- leads/urls.py
+++ leads/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'lead'
 
 urlpatterns = [
-    path('api/lead/', views.LeadListCreate.as_view(), name="leads"),
-    path('lead/', views.list_create_lead, name='getAllLeads'),
+    path('lead/', views.LeadListCreate.as_view(), name="leads"),
+    path('api/lead/', views.list_create_lead, name='getAllLeads'),
     path('lead/<int:leadId>', views.get_lead_by_id, name='getLeadById'),
 ]
